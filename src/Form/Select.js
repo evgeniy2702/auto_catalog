a8d37769fa652b1126afce29a5483c3064819caf
evgeniy2702 <evgeniy.zhurenko@gmail.com>
2021-03-14 21:08:28 +0200

    1 file added

---
+++ src/Form/Select.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./../style.css";
+import PropTypes from "prop-types";
+
+function Select({ elem, change }){
+
+  let type = "text";  
+  
+  return(
+    <div>
+      <label>{elem[2]} :</label>
+            <input
+              id = {elem[1]}
+              type= {type}
+              placeholder={"Enter your " + elem[1]}
+              value = {elem[0]}
+              onChange= {(e)=> change(elem[1],e.target.value )}
+            />
+            <br />
+            <br />
+  
+    </div>
+  )
+}
+
+Input.propTypes = {
+  elem: PropTypes.array.isRequired,
+  change: PropTypes.func.isRequired
+}
+export default Select;
